61baea23bd9e83253e76a625d3b88239fdffbd94
salmonco <xkqjsslsek80@naver.com>
2024-03-13 19:11:50 +0900

    Chore: package version update

--- next.config.js
+++ next.config.js
@@ -1,22 +1,12 @@
-/** @type {import('next').NextConfig} */
-const withPlugins = require("next-compose-plugins");
-const withPWA = require("next-pwa");
+// /** @type {import('next').NextConfig} */
+const runtimeCaching = require("next-pwa/cache");
 
-const isProduction = process.env.NODE_ENV === "production";
 const nextConfig = {};
 
-module.exports = withPlugins(
-  [
-    [
-      withPWA,
-      {
-        pwa: {
-          dest: "public",
-          disable: !isProduction,
-        },
-      },
-    ],
-    // 추가 플러그인 작성
-  ],
-  nextConfig
-);
+const withPWA = require("next-pwa")({
+  runtimeCaching,
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+});
+
+module.exports = withPWA(nextConfig);
