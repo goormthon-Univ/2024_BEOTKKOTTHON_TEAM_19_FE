a32be611b9145e87b1fe3785cee067eb2b6bc217
Jun279 <33.beautifulboy@gmail.com>
2024-03-21 18:09:36 +0900

    loadingPage 작업

---
+++ src/app/createHabit/create-loading.js
@@ -0,0 +1,9 @@
+import classes from "./create-loading.module.css";
+
+export default function CreateLoadingPage() {
+  return (
+    <>
+      <p className={classes.loadingText}>나무를 심기 위해 <br /> 열심히 흙을 파고 있어요</p>
+    </>
+  )
+}
