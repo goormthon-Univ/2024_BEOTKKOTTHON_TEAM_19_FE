ef3699fe9e7a85fe4e6184bcf3093b76b026830a
salmonco <xkqjsslsek80@naver.com>
2024-03-21 10:41:28 +0900

    Design: 첫 실행화면에 시작하기 버튼 추가

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,14 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col gap-5 items-center p-24">
+    <main className="flex flex-col justify-between items-center p-[20px] h-[100vh]">
       <h1>원해빗</h1>
+      <Link className="w-full" href="/login">
+        <button className="w-full disabled:bg-[#E5E5E5] disabled:text-[#999999] bg-[#41C364] text-white text-[1.4rem] font-[Pretendard-Bold] h-[53px] rounded-[6px]">
+          시작하기
+        </button>
+      </Link>
     </main>
   );
 }
