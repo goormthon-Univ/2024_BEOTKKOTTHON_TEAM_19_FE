9cb9b9d6d1e65e5f82fec2bfce4a89f8b1c742c2
Jun279 <33.beautifulboy@gmail.com>
2024-03-23 00:16:13 +0900

    인증글 버튼 추가

--- src/pages/habitDetail/index.js
+++ src/pages/habitDetail/index.js
@@ -1,8 +1,9 @@
 "use client";
 import classes from "./page.module.css";
 import HabitDetailComponent from "../../components/habit/habitDetailComponent.js";
-import Navbar from "../../components/nav/navBar.js";
-import CertificationComponent from "../../components/certification/certificationComponent.js";
+import Navbar from "../../components/nav/navBar";
+import CertificationComponent from "../../components/certification/certificationComponent";
+import Image from "next/image";
 import { useState } from "react";
 
 export default function HabitDetailPage() {
@@ -14,16 +15,18 @@ export default function HabitDetailPage() {
   // }
 
   return (
-    <>
+    <div className={classes.bodyContainer}>
       <div className={classes.topContainer}>
         <HabitDetailComponent textNumber={textNumber} />
       </div>
       <p className={classes.certificationText}>인증글</p>
       {certificationNumber.map((number, index) => {
-        <CertificationComponent key={index} />;
+        return <CertificationComponent key={index}/>
       })}
-      <div className={classes.addBtn}></div>
+      <div className={classes.addBtn}>
+        <Image src="/image/addBtn.png" alt="addBtn img" width={15} height={15} />
+      </div>
       <Navbar />
-    </>
-  );
+    </div>
+  )
 }
