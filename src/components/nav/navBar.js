0cdef401578271f9090d19dae05d70139a87920b
salmonco <xkqjsslsek80@naver.com>
2024-03-24 01:55:22 +0900

    Design: pathname으로 navBar 아이콘 강조색 설정

--- src/components/nav/navBar.js
+++ src/components/nav/navBar.js
@@ -1,8 +1,11 @@
 import Image from "next/image";
 import Link from "next/link";
 import classes from "./navBar.module.css";
+import { useRouter } from "next/router";
 
 export default function NavBar() {
+  const router = useRouter();
+
   return (
     <div className={classes.navContainer}>
       <div className="grid grid-cols-3 w-full justify-items-center">
@@ -12,12 +15,24 @@ export default function NavBar() {
             className="flex flex-col items-center px-[16px] gap-[4px]"
           >
             <Image
-              src={require("../../../public/image/home.svg")}
+              src={
+                router.pathname === "/mainHome"
+                  ? require("../../../public/image/home-green.svg")
+                  : require("../../../public/image/home.svg")
+              }
               alt="내 습관 보기"
               width={18}
               height={18}
             />
-            <p className={classes.navText}>홈</p>
+            <p
+              className={`font-[Pretendard-Medium] text-[1.4rem] ${
+                router.pathname === "/mainHome"
+                  ? "text-[#41C364]"
+                  : "text-[#999999]"
+              }`}
+            >
+              홈
+            </p>
           </Link>
         </div>
         <div className={classes.imgBox}>
@@ -26,12 +41,24 @@ export default function NavBar() {
             className="flex flex-col items-center px-[16px] gap-[4px]"
           >
             <Image
-              src={require("../../../public/image/garden.svg")}
+              src={
+                router.pathname === "/userList"
+                  ? require("../../../public/image/garden-green.svg")
+                  : require("../../../public/image/garden.svg")
+              }
               alt="다른 유저 습관 보기"
               width={18}
               height={18}
             />
-            <p className={classes.navText}>둘러보기</p>
+            <p
+              className={`font-[Pretendard-Medium] text-[1.4rem] ${
+                router.pathname === "/userList"
+                  ? "text-[#41C364]"
+                  : "text-[#999999]"
+              }`}
+            >
+              둘러보기
+            </p>
           </Link>
         </div>
         <div className={classes.imgBox}>
@@ -40,12 +67,24 @@ export default function NavBar() {
             className="flex flex-col items-center px-[16px] gap-[4px]"
           >
             <Image
-              src={require("../../../public/image/history.svg")}
+              src={
+                router.pathname === "/myHistory"
+                  ? require("../../../public/image/history-green.svg")
+                  : require("../../../public/image/history.svg")
+              }
               alt="내 인증글 보기"
               width={18}
               height={18}
             />
-            <p className={classes.navText}>히스토리</p>
+            <p
+              className={`font-[Pretendard-Medium] text-[1.4rem] ${
+                router.pathname === "/myHistory"
+                  ? "text-[#41C364]"
+                  : "text-[#999999]"
+              }`}
+            >
+              히스토리
+            </p>
           </Link>
         </div>
       </div>
