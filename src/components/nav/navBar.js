7de2707fbc06f0310fcf599cb24a8d23b5d66e3c
salmonco <xkqjsslsek80@naver.com>
2024-03-23 16:46:37 +0900

    Design: navBar 레이아웃을 grid로 변경

--- src/components/nav/navBar.js
+++ src/components/nav/navBar.js
@@ -5,23 +5,49 @@ import classes from "./navBar.module.css";
 export default function NavBar() {
   return (
     <div className={classes.navContainer}>
-      <div className={classes.imgBox}>
-        {/* <Link> */}
-        <Image src="/image/Home.png" alt="Home icon" width={18} height={18} />
-        <p className={classes.navText}>홈</p>
-        {/* </Link> */}
-      </div>
-      <div className={classes.imgBox}>
-        <Link href="/userList">
-          {/* <Image src="/image/Home.png" alt="Home icon" width={18} height={18}/> */}
-          <p className={classes.navText}>둘러보기</p>
-        </Link>
-      </div>
-      <div className={classes.imgBox}>
-        <Link href="/myHistory">
-          {/* <Image src="/image/Home.png" alt="Home icon" width={18} height={18}/> */}
-          <p className={classes.navText}>히스토리</p>
-        </Link>
+      <div className="grid grid-cols-3 w-full justify-items-center">
+        <div className={classes.imgBox}>
+          <Link
+            href="/mainHome"
+            className="flex flex-col items-center px-[16px]"
+          >
+            <Image
+              src="/image/Home.png"
+              alt="Home icon"
+              width={18}
+              height={18}
+            />
+            <p className={classes.navText}>홈</p>
+          </Link>
+        </div>
+        <div className={classes.imgBox}>
+          <Link
+            href="/userList"
+            className="flex flex-col items-center px-[16px]"
+          >
+            <Image
+              src="/image/Home.png"
+              alt="Home icon"
+              width={18}
+              height={18}
+            />
+            <p className={classes.navText}>둘러보기</p>
+          </Link>
+        </div>
+        <div className={classes.imgBox}>
+          <Link
+            href="/myHistory"
+            className="flex flex-col items-center px-[16px]"
+          >
+            <Image
+              src="/image/Home.png"
+              alt="Home icon"
+              width={18}
+              height={18}
+            />
+            <p className={classes.navText}>히스토리</p>
+          </Link>
+        </div>
       </div>
     </div>
   );
