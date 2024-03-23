92463b5a44d1bdc2dce127b3db9046ffba148ddc
salmonco <xkqjsslsek80@naver.com>
2024-03-23 19:34:13 +0900

    Feat: 무한 스크롤링 적용

--- src/pages/userList.js
+++ src/pages/userList.js
@@ -11,14 +11,12 @@ export default function UserList() {
     userInfo: { accessToken },
   } = useUserInfo();
   const [treeList, setTreeList] = useState([]);
-  const [fetchingData, setFetchingData] = useState([]);
   const [loading, setLoading] = useState(false);
-  // const [isEnd, setIsEnd] = useState(false);
+  const [isLastPage, setIsLastPage] = useState(false);
 
   const getTreeList = useCallback(async () => {
-    if (loading) return; // 이미 로딩 중이면 중복 호출 방지
-    setLoading(true); // 로딩 시작
     try {
+      setLoading(true); // 데이터 요청 시작 시 로딩 상태 설정
       const res = await axios.get("/api/users/tree-list", {
         headers: {
           authorization: `Bearer ${accessToken}`,
@@ -29,51 +27,28 @@ export default function UserList() {
         alert(message);
         return;
       }
-      if (data.length === 0) {
-        setIsEnd(true);
-        return;
-      }
       setTreeList((prev) => [...prev, ...data]);
-      setFetchingData(data);
+      setIsLastPage(data.length === 0);
     } catch (e) {
       console.error("Error fetching tree list:", e);
     } finally {
-      setLoading(false); // 로딩 종료
+      setLoading(false); // 데이터 요청 완료 시 로딩 상태 제거
     }
-  }, [accessToken, loading]);
+  }, [accessToken]);
 
   // useIntersect훅에 타겟 감지 시 실행해야할 콜백함수 전달
   const ref = useIntersect((entry, observer) => {
-    // 데이터 요청 실패 시 연속 호출을 막기 위해 타겟 해제
-    observer.unobserve(entry.target);
-
     // 불러올 데이터가 더 이상 없는지 체크
-    const isLastPage = treeList.length === fetchingData.length;
-    if (!isLastPage && !loading) {
-      getTreeList();
-    }
+    if (loading || isLastPage) return;
+    getTreeList();
   });
-
-  const onScroll = useCallback(() => {
-    if (
-      window.innerHeight + document.documentElement.scrollTop ===
-      document.documentElement.offsetHeight
-    ) {
-      getTreeList(); // 스크롤이 페이지 맨 아래에 도달하면 추가 데이터 로딩
-    }
-  }, [getTreeList]);
 
   useEffect(() => {
     getTreeList();
   }, [getTreeList]);
 
-  useEffect(() => {
-    window.addEventListener("scroll", onScroll);
-    return () => window.removeEventListener("scroll", onScroll);
-  }, [getTreeList, onScroll]);
-
   return (
-    <div className="h-[100vh] overflow-y-auto">
+    <div className="h-[100vh] overflow-y-auto pb-[100px]">
       <p>다른 유저의 나무 둘러보기</p>
       <div>
         {treeList.map((v) => (
